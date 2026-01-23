f5ab0c74fc16f361c1e69e02c255027b8ce81ad5
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2026-01-23 15:15:19 +0800

    lvl3

--- src/main/java/Hachiware.java
+++ src/main/java/Hachiware.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Hachiware {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> taskList = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
         System.out.println("----------------------------");
         System.out.println("Woooi! I'm Hachiware!");
         System.out.println("What can I help you with?");
@@ -12,25 +12,44 @@ public class Hachiware {
         while (true) {
             System.out.print("Input > ");
             String command = sc.nextLine();
-
+            
             if (command.equals("bye")) {
                 break;
             }
             else if (command.equals("list")) {
                 printTasks(taskList);
             }
+            else if (command.split(" ")[0].equals("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                Task tempTask = taskList.get(index);
+                tempTask.markDone();
+                taskList.set(index, tempTask);
+
+                System.out.println("Yay! I've ticked off this task:");
+                System.out.println(tempTask.toString());
+            }
+            else if (command.split(" ")[0].equals("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                Task tempTask = taskList.get(index);
+                tempTask.markNotDone();
+                taskList.set(index, tempTask);
+
+                System.out.println("Oops! I've unmarked this task:");
+                System.out.println(tempTask.toString());
+            }
             else {
-                taskList.add(command);
+                taskList.add(new Task(command));
                 System.out.println("Added: " + command);
             }
+            System.out.println("----------------------------");
         }
         
         System.out.println("Bye then!");
     }
 
-    public static void printTasks(List taskList) {
+    public static void printTasks(List<Task> taskList) {
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println(i+1 + ". " + taskList.get(i));
+            System.out.println(i+1 + "." + taskList.get(i).toString());
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String markDone() {
+        this.isDone = true;
+        return this.toString();
+    }
+    
+    public String markNotDone() {
+        this.isDone = false;
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.isDone? "X":" ") + "] " + this.description;
+    }
+}
