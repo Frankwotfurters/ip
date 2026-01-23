80f8841e70fa06dcd6bd863bfb8a03c479c4cf7d
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2026-01-23 14:11:01 +0800

    lvl0

---
+++ src/main/java/Hachiware.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Hachiware {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------------------------");
+        System.out.println("Woooi! I'm Hachiware!");
+        System.out.println("What can I help you with?");
+        System.out.println("----------------------------");
+        System.out.println("Bye then!");
+        
+    }
+}
