cc1cd5ab1e15d4f3e1ca88d6d4641f748329ec74
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2026-01-23 14:37:19 +0800

    lvl2

--- src/main/java/Hachiware.java
+++ src/main/java/Hachiware.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Hachiware {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> taskList = new ArrayList<>();
         System.out.println("----------------------------");
         System.out.println("Woooi! I'm Hachiware!");
         System.out.println("What can I help you with?");
@@ -15,11 +16,21 @@ public class Hachiware {
             if (command.equals("bye")) {
                 break;
             }
+            else if (command.equals("list")) {
+                printTasks(taskList);
+            }
             else {
-                System.out.println(command);
+                taskList.add(command);
+                System.out.println("Added: " + command);
             }
         }
         
         System.out.println("Bye then!");
+    }
+
+    public static void printTasks(List taskList) {
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(i+1 + ". " + taskList.get(i));
+        }
     }
 }
